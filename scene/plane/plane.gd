6ef64ec55f69ee331f2bae1cd46fb1bb891aea39
Savaş Tanrıverdi <savas.tanriverdi@outlook.com>
2025-10-07 02:16:25 +0300

    Add Godot Doctor plugin with validation features
    - Created the Godot Doctor plugin to validate scenes in Godot.
    - Added core classes for validation conditions, results, and ranges (both float and int).
    - Implemented settings for user preferences, including debug options and dock position.
    - Developed utility functions for evaluating validation conditions and checking instance validity.
    - Included scene files for barriers and planes with associated scripts and animations.
    - Established a structured approach for error reporting and validation feedback.

---
+++ scene/plane/plane.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+const JUMP_POWER: float = -350.0
+
+func _ready() -> void:
+	print(_gravity)
+
+
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	
+	fly(delta)
+	
+	move_and_slide()
+	
+	if is_on_floor() == true:
+		die()
+
+func fly(delta:float) -> void:
+	velocity.y += _gravity * delta
+	
+	if Input.is_action_just_pressed("jump") == true:
+		velocity.y = JUMP_POWER
+		animation_player.play("jump")
+
+
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
